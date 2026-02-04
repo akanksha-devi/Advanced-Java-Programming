260decb9ced02e713eda77592afff60403371a19
Akanksha Devi <akankshapundir13@gmail.com>
2026-02-04 16:53:43 +0000

    Interface usage

---
+++ AreaMain.java
@@ -0,0 +1,37 @@
+interface Area{
+    double PI = 3.14;
+    double getArea();
+}
+
+class Circle implements Area{
+    double r;
+    Circle(double r){
+        this.r = r;
+    }
+    public double getArea(){
+        return (PI*r*r);
+    }
+    void display(){
+        System.out.println("Area of circle: " + getArea());
+    }
+}
+
+public class AreaMain{
+    public static void main(String[] args){
+        Circle c1 = new Circle(12.5);
+        c1.display();
+        //System.out.println("Area of circle: "+ c1.getArea());
+
+        Area c2 = new Circle(13.5);
+        //c2.display();  //it voilates the rule of polymorphism
+        System.out.println("Area of circle: " + c2.getArea());
+
+        Area r1 = new Area(){
+            public double getArea(){
+                double l = 6, b = 8;
+                return (l*b);
+            }
+        };
+        System.out.println("Area of rectangle: " + r1.getArea());
+    }
+}
